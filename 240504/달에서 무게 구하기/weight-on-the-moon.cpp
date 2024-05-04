d8357b75847f26aec3e15a9b77510eb9ae028411
Iubar <101549899+ClarusIubar@users.noreply.github.com>
2024-05-04 21:12:10 +0900

    Title: 달에서 무게 구하기, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240504/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << fixed;
+    int a = 13;
+    float b = 0.165000;
+    
+    // cout.precision(6);
+    cout << a << " * " << b << " = " << a*b;
+ 
+    return 0;
+}
