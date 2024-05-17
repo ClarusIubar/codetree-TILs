927e0339fd0a3f7619b24104b86c7499652fda50
Iubar <101549899+ClarusIubar@users.noreply.github.com>
2024-05-17 22:50:07 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240517/두 사람/two-person.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int age1, age2;
+    char gender1, gender2;
+    scanf("%d %c", &age1, &gender1, &age2, &gender2);
+
+    if((age1>=19 && gender1 =='M') || (age2>=19 && gender2=='M')){
+        printf("%d", 1);
+    }
+    else{
+        printf("%d", 0);
+    }
+    return 0;
+}
