857cc14700f57e54fd7bebb9c4e937171ea8abc6
Iubar <101549899+ClarusIubar@users.noreply.github.com>
2024-05-17 23:40:46 +0900

    Title: 중앙값 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240517/중앙값 구하기/find-the-median.c
+++ 240517/중앙값 구하기/find-the-median.c
@@ -4,14 +4,12 @@ int main() {
     int a, b, c;
     scanf("%d %d %d", &a, &b, &c);
 
-    if(b>a && b<c){
-        printf("%d", b);
-    }
-    if(c>a && c<b){
-        printf("%d", c);
-    }
-    if(a>b && a<c){
-        printf("%d", a);
+    if ((a >= b && a <= c) || (a <= b && a >= c)) {
+        printf("%d",a);
+    } else if ((b >= a && b <= c) || (b <= a && b >= c)) {
+        printf("%d",b);
+    } else {
+        printf("%d",c);
     }
 
     return 0;
