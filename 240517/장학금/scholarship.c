2de67bd0c00540173b2250cdbe7ff6b50c172ff9
Iubar <101549899+ClarusIubar@users.noreply.github.com>
2024-05-17 22:39:32 +0900

    Title: 장학금, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240517/장학금/scholarship.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    if(a >= 90){
+        if(b >= 95){
+            printf("%d", 100000);
+        }
+        else if(b >= 90){
+            printf("%d", 50000);
+        }
+        else{
+            printf("%d", 0);
+        }
+    }
+    else{
+       printf("%d", 0);
+    }
+
+    return 0;
+}
